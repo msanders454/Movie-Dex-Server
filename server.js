4db4c659b003c2046ca6dd9fb05d9a178aab2002
msanders454 <50176010+msanders454@users.noreply.github.com>
2020-03-06 19:57:39 -0500

    Added Empty Query Problem

--- server.js
+++ server.js
@@ -8,11 +8,12 @@ const helmet = require('helmet');
 const app = express();
 const movies = require('./movies-data-small');
 
-app.use(morgan('dev')); 
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(cors())
 app.use(helmet()); 
 
-app.use(function validateBearerToken(req, res, next) {
+function validateBearerToken(req, res, next) {
   
     const apiToken = process.env.API_TOKEN
     console.log(apiToken)
@@ -21,39 +22,61 @@ app.use(function validateBearerToken(req, res, next) {
       return res.status(401).json({ error: 'Unauthorized request' })
     }
     next()
-  })
+  }
 
-app.get('/movie', function handleGetMovie(req, res) {
+app.get('/movie', [validateBearerToken], function (req, res) {
     let response = movies;
+    console.log(req.query);
 
-  
+    if (!Object.keys(req.query).length) {
+      console.log('wait');
+      res.json(response);
+      return 
+    }
+
     if (req.query.genre) {
-        response = response.filter(movie =>
-          movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
-        )
+      responseGenre = response.filter(movie =>
+        movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+        );
       }
-    else return res.status(400).json({ error: 'bad genre' });
+      else {
+        return res.status(400).json({ error: 'bad genre' });
+      }
   
 
     if (req.query.country) {
-        response = response.filter(movie =>
-          movie.country.toLowerCase().includes(req.query.country.toLowerCase())
-        )
-      }
-    else return res.status(400).json({ error: 'bad country' });
+      responseCountry = response.filter(movie =>
+        movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+        );
+    } else {
+      return res.status(400).json({ error: 'bad country' });
+    }
+
   
     if (req.query.avg_vote) {
-        response = response.filter(movie =>
-          Number(movie.avg_vote) >= Number(req.query.avg_vote)
-        )
+      responseAvg = response.filter(movie =>
+        Number(movie.avg_vote) >= Number(req.query.avg_vote)
+        );
       }
-    else return res.status(400).json({ error: 'bad avg_vote' });
+      else {
+        return res.status(400).json({ error: 'bad avg_Vote' });
+      }
   
 
     res.json(response)
 })
 
-const PORT = 9000
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 9000
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
